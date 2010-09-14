88d2656ff23ef7e4b6308a059b2f7ddc24aa0481
David Adamson <Mr.Adamson@gmail.com>
2010-09-14 00:22:54 -0400

    added skype-calling functionality (util/wakeupcall.py, uses Skype4Py) -I found it on the Internet, and it's still a bit ugly. -added conference-calling: wakeupcall.call_all(phone_list, wav_list) -this will start a conference with all listed phones and play the wavs sequentially. added group_demo app: "group call 1234567890 4445556666" (etc) will play a file to the group of given numbers. beowulf app would call you back with a matching audio clip, if only you had a folderful of beowulf recordings...
    see notes in wakeupcall.py for getting Skype4Py working on a Mac.

--- apps/group_demo/controller.py
+++ apps/group_demo/controller.py
@@ -1,6 +1,6 @@
-from apps.beowulf.responses import KEYWORD, match_line
+from apps.group_demo.responses import KEYWORD, group_call
 from util.models import Log
-from datetime import datetime
+import mongoengine as ME
 
 #define the app's behavior in the form of test / response pairs
 
@@ -10,15 +10,14 @@ from datetime import datetime
 #(returning None is also evaluated as False)
 
 def is_new(message):
+    ME.connect("group_demo")
     return not message.is_read and not Log.objects.filter(m_id=message.id)
 
-def is_match_request(message):
+def is_group_call_request(message):
     if (message.text.lower().startswith(KEYWORD) and is_new(message)):
         return True
-    else: return False
-
 
 #Here associate tests with responses
 reactions = (
-                (is_match_request, match_line),
+                (is_group_call_request, group_call),
             )
