faa776a709558e5c40af6da10d0a8a2d2f811393
David Adamson <Mr.Adamson@gmail.com>
2010-10-27 14:07:34 -0400

    added a "multiplex"

--- settings_weekender.py
+++ settings_weekender.py
@@ -3,14 +3,19 @@ CONNECTIONS = (
                  (
                     'doctor.hrothgar@gmail',
                     'pygooglevoice',
-                    {'GV_USER':"doctor.hrothgar@gmail.com",'GV_PASSWORD':None}
+                    {'GV_USER':"doctor.hrothgar@gmail.com",'GV_PASSWORD':'obscurepgh'}
+                 ),
+                 (
+                    'askory@andrew',
+                    'pygooglevoice',    
+                    {'GV_USER':"askory@andrew.cmu.edu",'GV_PASSWORD':'w33k3nd3r'},
                  ),
               )
 
 #Set the interval to ping each connection for new Messages
 #format is an integer in seconds, or a tuple specifying a range
 #from which to wait a random number of seconds each time
-CHECK_INTERVAL = (60, 120)
+CHECK_INTERVAL = (20, 40)
 
 #Specify apps here, format is (app,[connection1,connection2...])
 #if no connections are listed, all connections will be checked by default
